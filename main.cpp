8a626f8fd7298de8624708de4888cb70eecbb48a
Jan-86 <JanS3ri4l841125@gmail.com>
2026-02-15 12:51:43 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace std::chrono;
+
+struct Data {
+	long a;
+
+#ifdef FIX_FALSE_SHARING
+  long padding[7]; // seemingly useless variables, yet they solve the false sharing problem
+#endif
+
+	long b; // sits on the same L1 cache line as the "a" member
+	
+	Data() : a(0l), b(0l) {
+		
+	}
+};
+
+const long nb_iter = 100000000l;
+
+void work(long &e) {
+  while(e<nb_iter)
+  {
+    e++;
+  }
+}
+
+int main() {
+	const auto tic = high_resolution_clock::now();
+    
+	Data d;
+	std::thread t1(work, ref(d.a));
+	std::thread t2(work, ref(d.b));
+
+	t1.join();
+	t2.join();
+	const auto toc = high_resolution_clock::now();
+	const std::chrono::duration<double, std::milli> elapsed = toc - tic;
+	
+	cout << "a = " << d.a << endl;
+	cout << "b = " << d.b << endl;
+	cout << "Computation time: " << elapsed.count() << "ms" << endl;
+	
+	return 0;
+}
